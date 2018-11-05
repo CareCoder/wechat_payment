775948bac9f968f4eca8b3dc776456ee5bbb50cd
SongHuan <112597649@qq.com>
2018-11-05 16:40:07 +0800

    [dev] mybatis配置

--- src/main/java/com/itstyle/config/MyBatisConfig.java
+++ src/main/java/com/itstyle/config/MyBatisConfig.java
@@ -1,4 +1,9 @@
 package com.itstyle.config;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.itstyle.mapper")
 public class MyBatisConfig {
 }
