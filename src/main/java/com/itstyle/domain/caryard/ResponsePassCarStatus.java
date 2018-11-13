aeed00f6476626504672fa21717dd20ff2de33fa
SongHuan <112597649@qq.com>
2018-11-13 22:37:16 +0800

    [dev] 权限设置

--- src/main/java/com/itstyle/domain/caryard/ResponsePassCarStatus.java
+++ src/main/java/com/itstyle/domain/caryard/ResponsePassCarStatus.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class ResponsePassCarStatus {
     private Long id;
+    private String channelName;
     private Long channelTypeId;
     private Integer tempCarA;
     private Integer tempCarB;
