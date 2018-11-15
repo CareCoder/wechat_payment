46b1eb4451f34874687648b513a45b2b0d39b793
yq <120370694@qq.com>
2018-11-15 11:47:07 +0800

    [dev]临时车收费界面完成,接下来做下载的service,新增carType判断

--- src/main/java/com/itstyle/control/CarNumController.java
+++ src/main/java/com/itstyle/control/CarNumController.java
@@ -7,10 +7,7 @@ import com.itstyle.service.CarNumService;
 import com.itstyle.utils.enums.Status;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
@@ -21,6 +18,11 @@ import java.util.List;
 public class CarNumController {
     @Resource
     private CarNumService carNumService;
+
+    @GetMapping("/tempcarinfo.html")
+    public String tempcarinfo() {
+        return "/backend/tempcarinfo";
+    }
 
     @RequestMapping("/upload")
     @ResponseBody
