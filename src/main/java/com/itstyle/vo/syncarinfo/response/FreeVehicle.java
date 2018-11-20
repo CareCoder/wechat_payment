657c7c54c903bd304c7009073397c7ea940fc568
yq <120370694@qq.com>
2018-11-20 16:12:02 +0800

    [dev]所有的枚举返回索引值

--- src/main/java/com/itstyle/vo/syncarinfo/response/FreeVehicle.java
+++ src/main/java/com/itstyle/vo/syncarinfo/response/FreeVehicle.java
@@ -6,12 +6,12 @@ import lombok.Data;
 @Data
 public class FreeVehicle {
     private String freeVehicle_plate;//	免费车辆的车牌号
-    private String freeVehicle_plateColor;//	免费车辆的车牌颜色(蓝/黄/黑)
+    private Integer freeVehicle_plateColor;//	免费车辆的车牌颜色(蓝/黄/黑)
 
     public static FreeVehicle convert(CarInfo carInfo) {
         FreeVehicle fv = new FreeVehicle();
         fv.freeVehicle_plate = carInfo.getCarNum();
-        fv.freeVehicle_plateColor = carInfo.getCarColor() != null ? carInfo.getCarColor().toString() : "";
+        fv.freeVehicle_plateColor = carInfo.getCarColor() != null ? carInfo.getCarColor().ordinal() : null;
         return fv;
     }
 
--- src/main/java/com/itstyle/vo/syncarinfo/response/MonlyCarInfo.java
+++ src/main/java/com/itstyle/vo/syncarinfo/response/MonlyCarInfo.java
@@ -16,13 +16,13 @@ public class MonlyCarInfo {
 
     private String monlyCardTimeEnd;//时间格式： yyyy/MM/dd HH:mm:ss
 
-    private String monlyCarPlateColor;//月卡车辆车牌颜色(蓝/黄/黑)
+    private Integer monlyCarPlateColor;//月卡车辆车牌颜色(蓝/黄/黑)
 
     public static MonlyCarInfo convert(MonthCarInfo mc) {
         MonlyCarInfo ml = new MonlyCarInfo();
         ml.monlyCardTimeEnd = DateUtil.format(mc.getEndTime());
         ml.monlyCardTimeStart = DateUtil.format(mc.getStartTime());
-        ml.monlyCarPlateColor = mc.getCarColor() != null ? mc.getCarColor().toString() : "";
+        ml.monlyCarPlateColor = mc.getCarColor() != null ? mc.getCarColor().ordinal() : null;
         ml.name = mc.getName();
         ml.phoneNumber = mc.getPhone();
         ml.plateID = mc.getIdCardNum();
