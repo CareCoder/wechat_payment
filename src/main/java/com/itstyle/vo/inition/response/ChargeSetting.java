961a10d94ca1a8ce878c2e1425714994f0a689df
yq <120370694@qq.com>
2018-11-20 15:47:11 +0800

    [dev]初始化数据对象创建

---
+++ src/main/java/com/itstyle/vo/inition/response/ChargeSetting.java
@@ -0,0 +1,5 @@
+package com.itstyle.vo.inition.response;
+
+public class ChargeSetting {
+    public int chargeModel;//	1:按次收费；2：标准停车场收费；3：深圳写字楼收费；4白天夜间收费标准
+}
---
+++ src/main/java/com/itstyle/vo/inition/response/Inition.java
@@ -0,0 +1,11 @@
+package com.itstyle.vo.inition.response;
+
+import com.itstyle.vo.syncarinfo.response.SynCarInfo;
+
+public class Inition {
+    public ParkingSetup parkingSetup;
+    public AccessAuthoritySetup accessAuthoritySetup;
+    public ChargeSetting chargeSetting;
+    public ChargeRule chargeRule;
+    public SynCarInfo synCarInfo;
+}
