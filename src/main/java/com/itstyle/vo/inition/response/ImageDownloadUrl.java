124f05d63a29ea2e4eb4a7171462de2ca862be21
yq <120370694@qq.com>
2018-12-18 16:36:32 +0800

    [dev] lcd信息发布

--- src/main/java/com/itstyle/vo/inition/response/ImageDownloadUrl.java
+++ src/main/java/com/itstyle/vo/inition/response/ImageDownloadUrl.java
@@ -2,4 +2,11 @@ package com.itstyle.vo.inition.response;
 
 public class ImageDownloadUrl {
     public String imageDownloadUrl;//图片下载的地址
+
+    public ImageDownloadUrl() {
+    }
+
+    public ImageDownloadUrl(String imageDownloadUrl) {
+        this.imageDownloadUrl = imageDownloadUrl;
+    }
 }
