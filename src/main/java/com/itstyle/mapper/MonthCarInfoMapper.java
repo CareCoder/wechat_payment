563d3e5b7276c289150fcb6b29ff308ecbbccd35
1282901262 <1282901262@qq.com>
2018-12-21 10:07:56 +0800

    添加了获取指定车辆信息方法，及用于区分是否为月租车方法

--- src/main/java/com/itstyle/mapper/MonthCarInfoMapper.java
+++ src/main/java/com/itstyle/mapper/MonthCarInfoMapper.java
@@ -4,6 +4,16 @@ import com.itstyle.domain.car.manager.MonthCarInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.List;
+
 
 public interface MonthCarInfoMapper extends JpaRepository<MonthCarInfo, Long>, JpaSpecificationExecutor<MonthCarInfo> {
+    /**
+     * 根据车牌号获取指定车辆信息
+     * @param carNum
+     * @return
+     */
+    MonthCarInfo findByCarNum(String carNum);
+
+    List<MonthCarInfo> findByIsMonth(boolean b);
 }
