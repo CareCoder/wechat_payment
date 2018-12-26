773fe70817e6f612df5d8e2238e66e99f4d7bc79
yq <120370694@qq.com>
2018-12-26 10:54:59 +0800

    [dev]登录日志导出excel

---
+++ src/main/java/com/itstyle/domain/car/manager/LogExcelModel.java
@@ -0,0 +1,33 @@
+package com.itstyle.domain.car.manager;
+
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.metadata.BaseRowModel;
+import com.itstyle.domain.log.SysLoggerResponse;
+import com.itstyle.utils.DateUtil;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class LogExcelModel extends BaseRowModel{
+    @ExcelProperty(value = "用户名" ,index = 0)
+    private String username;
+    @ExcelProperty(value = "操作" ,index = 1)
+    private String action;
+    @ExcelProperty(value = "描述" ,index = 2)
+    private String desc;
+    @ExcelProperty(value = "操作时间" ,index = 3)
+    private String logDate;
+    @ExcelProperty(value = "操作员" ,index = 4)
+    private String roleName;
+
+    public static LogExcelModel convert(SysLoggerResponse c) {
+        LogExcelModel m = new LogExcelModel();
+        m.setUsername(c.getUsername());
+        m.setAction(c.getAction());
+        m.setDesc(c.getTDescribe());
+        m.setLogDate(DateUtil.format(c.getLogDate().getTime()));
+        m.setRoleName(c.getRoleName());
+        return m;
+    }
+}
--- src/main/java/com/itstyle/service/LogService.java
+++ src/main/java/com/itstyle/service/LogService.java
@@ -4,10 +4,13 @@ import com.itstyle.common.PageResponse;
 import com.itstyle.domain.log.SysLoggerResponse;
 
 import java.util.Date;
+import java.util.List;
 
 public interface LogService {
 
     Integer doSystemLogSave();
 
     PageResponse<SysLoggerResponse> list(int page, int limit, Date start, Date end, Long roleID);
+
+    List<SysLoggerResponse> list();
 }
