0edaf694b0bb4a3ee1cdde52aca7681f9f0a5122
yq <120370694@qq.com>
2019-04-23 17:40:36 +0800

    [dev]删除注释

--- src/main/java/com/itstyle/domain/car/manager/ChargeRecordExcelModel.java
+++ src/main/java/com/itstyle/domain/car/manager/ChargeRecordExcelModel.java
@@ -12,63 +12,24 @@ import java.util.List;
 
 @Data
 public class ChargeRecordExcelModel  extends BaseRowModel{
-    /**
-     * 车牌号码
-     */
     @ExcelProperty(value = "车牌号码" ,index = 0)
     private String carNum;
-
-    /**
-     * 入场时间
-     */
     @ExcelProperty(value = "入场时间" ,index = 1)
     private String enterTime;
-
-    /**
-     * 出场时间
-     */
     @ExcelProperty(value = "出场时间" ,index = 2)
     private String leaveTime;
-    /**
-     * 车辆真实类型
-     */
     @ExcelProperty(value = "车辆类型" ,index = 3)
     private String carRealType;
-
-    /**
-     * 应收金额
-     */
     @ExcelProperty(value = "车牌号码" ,index = 4)
     private Integer receivableFee;
-
-    /**
-     * 优惠金额
-     */
     @ExcelProperty(value = "优惠金额" ,index = 5)
     private Integer discountAmount;
-
-    /**
-     * 实收金额
-     */
     @ExcelProperty(value = "实收金额" ,index = 6)
     private Integer fee;
-
-    /**
-     * 收费类型
-     */
     @ExcelProperty(value = "收费类型" ,index = 7)
     private String chargeType;
-
-
-    /**
-     * 收费情况
-     */
     @ExcelProperty(value = "收费情况" ,index = 8)
     private String chargeSituation;
-
-    /**
-     * 收费员
-     */
     @ExcelProperty(value = "收费员" ,index = 9)
     private String chargePersonnel;
 
